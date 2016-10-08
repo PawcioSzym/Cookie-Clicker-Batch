9dce23b2e8bba08fb47f25071864f1da055da94f
MrCyjaneK <MrCyjaneK@Gmail.com>
2016-10-08 18:17:54 +0200

    Update Loader.bat

--- Loader.bat
+++ Loader.bat
@@ -1,18 +1,8 @@
 rem Loader
 @echo off
-echo Wpisz nazwe gracza (TAB uzytkownicy)
-md .NayC-R-M
-attrib +h .NayC-R-M
-cd .NayC-R-M
-md CC
-cd CC
-set /p Nazwa=:
-MD %Nazwa%
-cd %Nazwa%
-re Uruchamianie programów
 start Clicker.bat
 start shop.bat
-start Cps.bat
+start CpsCookies.bat
 exit
 
 
