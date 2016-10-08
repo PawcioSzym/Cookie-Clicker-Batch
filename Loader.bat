7476b40c3c94e235dd9918f3308f2034fcf5e3b8
MrCyjaneK <MrCyjaneK@Gmail.com>
2016-10-08 16:59:29 +0200

    Create Loader.bat

---
+++ Loader.bat
@@ -0,0 +1,18 @@
+rem Loader
+@echo off
+echo Wpisz nazwe gracza (TAB uzytkownicy)
+md .NayC-R-M
+attrib +h .NayC-R-M
+cd .NayC-R-M
+md CC
+cd CC
+set /p Nazwa=:
+MD %Nazwa%
+cd %Nazwa%
+re Uruchamianie programów
+
+
+
+
+
+
