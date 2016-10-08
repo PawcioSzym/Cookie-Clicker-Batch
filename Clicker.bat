c537d7052ffbb6f050400cebbb7b86b862a23783
MrCyjaneK <MrCyjaneK@Gmail.com>
2016-10-08 18:15:06 +0200

    Update Clicker.bat

--- Clicker.bat
+++ Clicker.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
-set /p Cookies=<"CookiesFille"
+set /p CookiesN=<"Cookies"
 :1
 del Cookies -y
-set /a wynik=%Cookies% + 1
-@echo %Cookies% > Cookies
+set /a wynik=%CookiesN% + 1
+@echo %CookiesN% > Cookies
 pause
 goto 1
 
